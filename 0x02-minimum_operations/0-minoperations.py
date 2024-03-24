8ede65d3f7724071eda41e3a0280ec1ed5e31723
Ernest-Geek <ernestampene1@gmail.com>
2024-03-24 17:48:31 +0000

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
+
 def minOperations(n):
+    """
+    Calculate the minimum number of operations needed to achieve n 'H' characters.
+
+    Args:
+        n (int): The target number of 'H' characters.
+
+    Returns:
+        int: The minimum number of operations needed.
+
+    Raises:
+        ValueError: If n is less than or equal to 1.
+
+    """
     if n <= 1:
-        return []
-    # initialize a list to store the minimum operation
-    dp = [0] * (n+1)
-    for i in range(2, n+1):
-        # initialize with the maximum number value
+        raise ValueError("n must be greater than 1")
+    
+    # Initialize a list to store the minimum operations
+    dp = [0] * (n + 1)
+    
+    for i in range(2, n + 1):
+        # Initialize with the maximum number value
         dp[i] = i
-        # try all possible divisors of i
+        
+        # Try all possible divisors of i
         for j in range(2, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
-
 
     return dp[n]

3f4100ae8df1b053affa319ab7d39e60b453b844
Ernest-Geek <ernestampene1@gmail.com>
2024-05-13 01:36:43 +0000

    Added island_perimeter function.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,15 +4,17 @@
 
     Args:
         grid (List[List[int]]): A list of lists representing the island grid,
-                                 where 0 represents water and 1 represents land.
+        where 0 represents water and 1 represents land.
 
     Returns:
         int: The perimeter of the island.
 """
+
+
 def island_perimeter(grid):
     perimeter = 0
     rows, cols = len(grid), len(grid[0])
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -21,5 +23,5 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
-    
+
     return perimeter
